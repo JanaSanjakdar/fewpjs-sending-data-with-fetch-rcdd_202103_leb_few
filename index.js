d32910876780d014759fb074cbae0202f37acf69
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-24 21:12:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let configObj = {
         },
         body: JSON.stringify(formData)
     };
-    return fetch("http://localhost:3000/users", configObj)
+  fetch("http://localhost:3000/users", configObj)
           .then(function(response) {
             return response.json();
           })
