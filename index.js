1271c8c3f24554e722095c1c264418f3ec0279d3
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-24 21:18:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ function submitData (name, email){
        name: name,
        email: email
    };
+   return fetch("http://localhost:3000/users", configObj)
+           .then(function(response) {
+             return response.json();
+           })
+           .then(function(object) {
+               let h2 = document.createElement('h2');
+               h2.innerHTML = object.id;
+               document.body.appendChild(h2);
+               console.log(object);
+           })
+           .catch(function(error) {
+               let h3 = document.createElement('h3');
+               h3.innerHTML = error.message;
+               document.body.appendChild(h3);
+               console.log(error.message);
+           });
 }
 
 let configObj = {
@@ -14,19 +30,4 @@ let configObj = {
         },
         body: JSON.stringify(formData)
     };
-  fetch("http://localhost:3000/users", configObj)
-          .then(function(response) {
-            return response.json();
-          })
-          .then(function(object) {
-              let h2 = document.createElement('h2');
-              h2.innerHTML = object.id;
-              document.body.appendChild(h2);
-              console.log(object);
-          })
-          .catch(function(error) {
-              let h3 = document.createElement('h3');
-              h3.innerHTML = error.message;
-              document.body.appendChild(h3);
-              console.log(error.message);
-          });
+  
